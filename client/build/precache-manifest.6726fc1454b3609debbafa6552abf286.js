250559d7c5f7acb575dc1249e4ea7d5f4a18bec7
robertmadril <robertmmadril@gmail.com>
2019-06-11 19:21:09 -0700

    commit to deploy

---
+++ client/build/precache-manifest.6726fc1454b3609debbafa6552abf286.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1f0473016bc9552d0a6",
+    "url": "/static/css/main.e3f36d63.chunk.css"
+  },
+  {
+    "revision": "f1f0473016bc9552d0a6",
+    "url": "/static/js/main.03085173.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08e60256e97de3e5b743",
+    "url": "/static/js/2.8006c6cb.chunk.js"
+  },
+  {
+    "revision": "7d41d9c723ecd6cd4bb18e78ecfe5121",
+    "url": "/index.html"
+  }
+];
